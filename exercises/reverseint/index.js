cb5615dab40d6ba01f938865bb9d31bd1f994a2e
Vlasis Charalampous <vlasischar@gmail.com>
2020-12-04 00:25:54 +0200

    feat: Completed Section 6: MaxChars

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// 1st solution
+function reverseInt(n) {
+    const reversed = n
+        .toString()
+        .split('')
+        .reverse()
+        .join('');
+
+    return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
